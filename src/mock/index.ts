7c5debd2234a8b1931c6a0e1542f807eea9eb1f6
agr0meow <neketa.erma13@gmail.com>
2023-07-10 12:38:28 +0300

    first-version
    first-version

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "agr0meow",
+  avatar: "https://avatars.githubusercontent.com/u/100200151?v=4",
+  name: "Nikita Ermakov",
+  company: "",
+  blog: "",
+  location: "Malaga",
+  bio: "Frontend Developer. ReactJS, React-Router, Redux/RTK, TypeScript,Node.js Next.js Tailwind,scss,sass",
+  twitter: "",
+  repos: 22,
+  followers: 2,
+  following: 2,
+  created: "2022-02-22T10:17:45Z",
+};
